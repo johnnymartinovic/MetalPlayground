6c5c156452007a77c765f369211d9d58e0d06c42
Ivan Martinović <martinovic1412@gmail.com>
2022-04-04 12:32:25 +0200

    Draw rectangle :)

--- MetalPlayground/ViewController.swift
+++ MetalPlayground/ViewController.swift
@@ -1,19 +1,93 @@
-//
-//  ViewController.swift
-//  MetalPlayground
-//
-//  Created by Ivan Martinović on 04.04.2022..
-//
-
 import UIKit
+import Metal
 
 class ViewController: UIViewController {
+    
+    var device: MTLDevice!
+    
+    var metalLayer: CAMetalLayer!
+    var vertexBuffer: MTLBuffer!
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    
+    var timer: CADisplayLink!
+    
+    let vertexData: [Float] = [
+        0.0,  0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0
+    ]
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupMetal()
+        
+        let dataSize = vertexData.count * MemoryLayout<Float>.size
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+
+        setupPipeline()
+        
+        commandQueue = device.makeCommandQueue()
+
+        timer = CADisplayLink(target: self, selector: #selector(gameloop))
+        timer.add(to: RunLoop.main, forMode: .default)
     }
 
-
+    private func setupMetal() {
+        device = MTLCreateSystemDefaultDevice()
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+    }
+    
+    private func setupPipeline() {
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+    
+    @objc private func gameloop() {
+      autoreleasepool {
+        self.render()
+      }
+    }
+    
+    private var green = 0
+    
+    private func render() {
+        guard let drawable = metalLayer?.nextDrawable() else { return }
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        green = (green + 1) % 256
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+          red: 0.0,
+          green: Double(green) / 255.0,
+          blue: 55.0 / 255.0,
+          alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+        
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
 }
 
